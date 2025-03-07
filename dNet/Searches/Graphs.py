ca79573ac9c22b3dbf6a196b6e51441aa9de38c3
Danishwer _ <dannywits12321@gmail.com>
2025-03-07 17:55:26 +0530

    Water Jug Problem Simulator

---
+++ dNet/Searches/Graphs.py
@@ -0,0 +1,104 @@
+import dearpygui.dearpygui as dpg
+import time
+
+
+class Node:
+    equalityIndexes = [0, 1]
+
+    def __init__(self, value, parent=None, children=None, ):
+        self.value = value
+        self.parent = parent
+        self.children = children if children else []
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+    def add_children(self, children):
+        for child in children:
+            child.parent = self
+            self.children.append(child)
+
+    def print_children(self):
+        print(self.children)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+    def __eq__(self, node):
+        if node == None:
+            return False
+        result = True
+        for i in self.equalityIndexes:
+            result = result and self.value[i] == node.value[i]
+        return result
+
+    def __hash__(self):
+        return hash((self.value[0], self.value[1]))
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = []
+
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def add_nodes(self, nodes):
+        self.nodes.extend(nodes)
+
+    def BFS(self, start, end):
+        queue = [start]
+        parents = {start: None}
+        while queue:
+            node = queue.pop(0)
+            if node.__eq__(end):
+                end = node
+                print(f"Found : {node.value}")
+                break
+            for child in possible_states(node):
+                if child in parents:
+                    continue
+                queue.append(child)
+                parents[child] = node
+        path = [end]
+        while parents[end] != None:
+            path.append(parents[end])
+            end = parents[end]
+        path.reverse()
+        print(path)
+        return path
+
+
+def possible_states(node):
+    water1 = node.value[0]
+    water2 = node.value[1]
+    capacity1 = node.value[2]
+    capacity2 = node.value[3]
+    states = []
+    if (water1 != capacity1):
+        states.append(
+            Node([capacity1, water2, capacity1, capacity2]))
+    if (water2 != capacity2):
+        states.append(
+            Node([water1, capacity2, capacity1, capacity2]))
+    if (water1 != 0):
+        states.append(Node([0, water2, capacity1, capacity2]))
+    if (water2 != 0):
+        states.append(Node([water1, 0, capacity1, capacity2]))
+    if (water1 + water2 <= capacity1):
+        states.append(
+            Node([water1 + water2, 0, capacity1, capacity2]))
+    if (water1 + water2 <= capacity2):
+        states.append(
+            Node([0, water1 + water2, capacity1, capacity2]))
+    if (water1 + water2 > capacity1):
+        states.append(
+            Node([capacity1, water1 + water2 - capacity1, capacity1, capacity2]))
+    if (water1 + water2 > capacity2):
+        states.append(
+            Node([water1 + water2 - capacity2, capacity2, capacity1, capacity2]))
+    return states
--- dNet/waterJugProblem.py
+++ dNet/waterJugProblem.py
@@ -1,10 +1,12 @@
 from dearpygui.dearpygui import *
-# rules
-
+from numpy import gcd
+from Searches.Graphs import *
+import time
 jugSize = 150
 bgColor = (255, 255, 255, 255)
 y = 200
 actions: list = []
+solution: list = []
 
 
 def addToAction(action: str):
@@ -72,7 +74,7 @@ class Jug:
         delete_item(self.lastFill)
 
 
-jug1 = Jug("Jug1", 0, 1, "drawList", x=50)
+jug1 = Jug("Jug1", 0, 5, "drawList", x=50)
 jug2 = Jug("Jug2", 0, 3, "drawList", x=80+jugSize)
 
 
@@ -86,6 +88,100 @@ def setJug2():
     jug2.draw()
 
 
+def solve():
+    if (jug1.capacity == 0 or jug2.capacity == 0):
+        addToAction("Invalid Input")
+        return
+    target = get_value("Target") if get_value(
+        "Target") != 0 else get_value("Target2")
+    Node.equalityIndexes = [0] if get_value("Target") != 0 else [1]
+    isFirstJug = get_value("Target") != 0
+    hcf = gcd(jug1.capacity, jug2.capacity)
+    print(f"Target: {target} jug: {Node.equalityIndexes[0]}")
+    if (target % hcf != 0):
+        addToAction("Can't Solve this state")
+        return
+    if (isFirstJug and target > jug1.capacity):
+        addToAction("Target is greater than\n Jug1 capacity")
+        return
+    elif (not isFirstJug and target > jug2.capacity):
+        addToAction("Target is greater than\n Jug2 capacity")
+        return
+    addToAction("Solving...")
+    g = Graph()
+
+    path = g.BFS(Node([jug1.water, jug2.water, jug1.capacity,
+                       jug2.capacity]), Node([target if isFirstJug else 0, target if not isFirstJug else 0, jug1.capacity, jug2.capacity]))
+    actionsToTake = []
+    for i in range(len(path)-1):
+        actionsToTake.append(get_action(path[i].value, path[i+1].value))
+    resetSolution()
+    for action in actionsToTake:
+        addToSolution(action)
+    addToAction("Solved")
+
+
+def addToSolution(text):
+    global solution
+    solution.append(text)
+    set_value("Solution", "Solution :\n"+"\n".join(solution))
+
+
+def resetSolution():
+    global solution
+    solution = []
+    set_value("Solution", "Solution :\n"+"\n".join(solution))
+
+
+def performSolution():
+    jug1.empty()
+    jug2.empty()
+    for action in solution:
+        time.sleep(0.7)
+        action = action[0]
+        if (action == "1"):
+            jug1.fill()
+        elif (action == "2"):
+            jug2.fill()
+        elif (action == "3"):
+            jug1.empty()
+        elif (action == "4"):
+            jug2.empty()
+        elif (action == "5"):
+            jug1.pour_to_other_until_full(jug2)
+        elif (action == "6"):
+            jug2.pour_to_other_until_full(jug1)
+
+
+def get_action(state1: list, state2: list):
+    initialWater1 = state1[0]
+    finalWater1 = state2[0]
+    capacity1 = state1[2]
+    capacity2 = state1[3]
+    initialWater2 = state1[1]
+    finalWater2 = state2[1]
+    # No Action
+    if (initialWater1 == finalWater1 and initialWater2 == finalWater2):
+        return "None"
+    if (initialWater1 != finalWater1 and initialWater2 != finalWater2):
+        if (initialWater1 > finalWater1):
+            return "5 | Pour Jug 1 to Jug 2"
+        else:
+            return "6 | Pour Jug 2 to Jug 1"
+    # Fill Jug 1
+    if (finalWater1 == capacity1):
+        return "1 | Fill Jug 1"
+    # Fill Jug 2
+    if (finalWater2 == capacity2):
+        return "2 | Fill Jug 2"
+    # Empty Jug 1
+    if (finalWater1 == 0):
+        return "3 |Empty Jug 1"
+    # Empty Jug 2
+    if (finalWater2 == 0):
+        return "4 | Empty Jug 2"
+
+
 # gui setup
 create_context()
 
@@ -93,46 +189,71 @@ create_context()
 with theme() as my_theme:
     with theme_component(mvWindowAppItem):
         add_theme_color(mvThemeCol_WindowBg, bgColor)
-
+description = """This is a simple Water Jug Problem
+Simulation and Solver.
+It uses BFS to solve the problem.
+Use the panels on the left to configure the problem.
+Press Solve to solve the problem
+Then Press Perform Solution to see the solution"""
+with window(label="Tutorial", tag="Tutorial"):
+    with drawlist(width=400, height=200, tag="drawList1"):
+        draw_text(text="Water Jug Problem", pos=(10, 10), size=24,
+                  color=(111, 45, 168, 255), tag="Title", parent="drawList1")
+        draw_text(text=description, pos=(10, 40), size=14,
+                  color=(0, 0, 0, 255), tag="Description", parent="drawList1")
 with window(label="Main", tag="Main"):
-    add_text(default_value="Water Jug Problem",
-             color=(0, 0, 0, 255), tag="Title")
-    add_text(default_value="Actions :",
+
+    add_text(default_value="Actions :", pos=(760, 30),
              color=(0, 0, 0, 255), tag="Actions")
-    add_button(label="Pop Action", callback=popAction)
+    add_text(default_value="Solution :",
+             pos=(280, 30),
+             color=(0, 0, 0, 255), tag="Solution")
+    add_button(label="Perform Solution", pos=(
+        280, 10), callback=performSolution)
+    add_button(label="Pop Action", pos=(760, 10), callback=popAction)
     with drawlist(width=400, height=400, tag="drawList"):
         pass
-with window(label="Jug 1 Controls", pos=(550, 0), width=200, height=150, tag="Controls1"):
-    add_slider_int(label="Capacity", min_value=1, max_value=10,
-                   tag="jug1Capacity")
-    add_button(label="Set Jug 1", callback=setJug1)
+with window(label="Jug 1 Controls", pos=(500, 0), width=250, height=150, tag="Controls1"):
+    add_slider_int(label="Capacity", min_value=1, max_value=20, default_value=5,
+                   tag="jug1Capacity", callback=setJug1)
+    add_text(default_value="Actions")
     add_button(label="Fill Jug 1", callback=jug1.fill)
     add_button(label="Empty Jug 1", callback=jug1.empty)
     add_button(label="Pour to Jug 2",
                callback=lambda: jug1.pour_to_other_until_full(jug2))
-with window(label="Jug 2 Controls", pos=(550, 200), width=200, height=150, tag="Controls2"):
-    add_slider_int(label="Capacity", min_value=1, max_value=10,
-                   tag="jug2Capacity")
-    add_button(label="Set Jug 2", callback=setJug2)
+with window(label="Jug 2 Controls", pos=(500, 200), width=250, height=150, tag="Controls2"):
+    add_slider_int(label="Capacity", min_value=1, max_value=20, default_value=3,
+                   tag="jug2Capacity", callback=setJug2)
+    add_text(default_value="Actions")
     add_button(label="Fill Jug 2", callback=jug2.fill)
     add_button(label="Empty Jug 2", callback=jug2.empty)
     add_button(label="Pour to Jug 1",
                callback=lambda: jug2.pour_to_other_until_full(jug1))
 
 bind_item_theme("Main", my_theme)
-
+bind_item_theme("Tutorial", my_theme)
+with window(label="Solver", pos=(500, 400), width=250, height=150, tag="Solver"):
+    add_text(default_value="Set target value\nIf Target1 is 0\nthen Target2 is used",
+             tag="TargetTitle")
+    add_slider_int(label="Target1", min_value=0, max_value=19,
+                   default_value=3, tag="Target")
+    add_slider_int(label="Target2", min_value=1, max_value=19,
+                   default_value=3, tag="Target2")
+    add_button(label="Solve", callback=solve)
 
 # starting the GUI
-create_viewport(title='2 Jug Problem', width=800, height=600)
+
+
+create_viewport(title='2 Jug Problem', width=1000, height=600)
 setup_dearpygui()
+bind_font(font)
 show_viewport()
 set_primary_window("Main", True)
-
-start_dearpygui()
-
-print(get_item_width("Main"))
-configure_item("drawList", width=get_item_width("Main"),
-               height=get_item_height("Main"))
 jug1.draw()
 jug2.draw()
+start_dearpygui()
+
+
+configure_item("drawList", width=get_item_width("Main"),
+               height=get_item_height("Main"))
 destroy_context()
